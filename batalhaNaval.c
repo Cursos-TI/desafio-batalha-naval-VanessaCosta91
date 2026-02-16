e7d0144a2348700a33c7b89e046bf9cc075c716d
VanessaCosta91 <van.nessacic@gmail.com>
2026-02-16 12:03:45 +0000

    adiciona navios nas diagonais

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
 
+# define LINHAS 10
+# define COLUNAS 10
+
 int main() {
     
 
-    char linha[10] = {'A','B','C','D','E','F','G','H','I','J'};    
-    int tabuleiro[10][10] = {0}; 
+    char linha[LINHAS] = {'A','B','C','D','E','F','G','H','I','J'};    
+    int tabuleiro[LINHAS][COLUNAS] = {0}; 
     int navioHorizontal[3] = {3,3,3};
     int navioVertical[3] = {3,3,3};
+    int diagonal1[3] = {3,3,3};
+    int diagonal2[3] = {3,3,3};
 
-    // Posição dos navios
+    // Coordenadas iniciais
+    int linhaH = 6, colunaH = 6; 
+    int linhaV = 5, colunaV = 1;
+    int linhaD1 = 0, colunaD1 = 0;
+    int linhaD2 = 0, colunaD2 = 9;
+    
+    // Diagonal principal 
+    for (int i=0; i<3; i++) {
+        tabuleiro[linhaD1+i][colunaD1+i] = diagonal1[i];
+    }
 
-    int linhaH = 2, colunaH = 4; 
-    int linhaV = 5, colunaV = 1; 
+    // Diagonal inversa
+    for (int i=0; i<3; i++) {
+        tabuleiro[linhaD2+i][colunaD2-i] = diagonal2[i];
+    }
 
+    // Horizontal
     for(int i = 0; i < 3; i++) {
         tabuleiro[linhaH][colunaH + i] = navioHorizontal[i];
     }
-
+    // Vertical
      for(int i = 0; i < 3; i++) {
         tabuleiro[linhaV + i][colunaV] = navioVertical[i];
     }
@@ -36,32 +53,6 @@ int main() {
             }
 
         printf("\n");
-    }
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
+    } 
     return 0;
 }
